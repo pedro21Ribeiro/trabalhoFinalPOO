45affa011fd0e47cb9fcc584b22891428e20f75a
pedro21ribeiro <pedr21ribeiro7@gmail.com>
2024-11-19 22:22:55 -0300

    EMergency Commit
    Machine running out of space

--- src/main/java/com/example/fatecpoo/Infra/Security/SecurityConfig.java
+++ src/main/java/com/example/fatecpoo/Infra/Security/SecurityConfig.java
@@ -45,13 +45,15 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/").permitAll() 
+                        .requestMatchers("/", "/filme/todos", "/static", "/public").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers("/v3/api-docs/**", "swagger-ui/**", "swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.GET).hasAnyAuthority("USER","ADMIN")
                         .anyRequest().hasAuthority("ADMIN")//authenticated()
                 )
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterAfter(securityFilter, UsernamePasswordAuthenticationFilter.class);
                 return http.build();
     }
 
